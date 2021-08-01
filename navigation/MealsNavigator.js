5183c7b8c06ee681b973de462f65ce85e08649aa
Kacper Zawojski <zawojweb@gmail.com>
2021-08-01 16:14:35 +0200

    different styles for differrent platforms

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -4,16 +4,27 @@ import { createStackNavigator } from '@react-navigation/stack';
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
 import MealDetailScreen from "../screens/MealDetailScreen"
-
+import Colors from '../constants/Colors';
+import { Platform } from 'react-native';
 const Stack = createStackNavigator();
 
 export default function MealsNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="CategoriesScreen" component={CategoriesScreen} />
-        <Stack.Screen name="CategoryMealsScreen" component={CategoryMealsScreen} />
-        <Stack.Screen name="MealDetailScreen" component={MealDetailScreen} />
+        <Stack.Screen 
+          name="CategoriesScreen" 
+          component={CategoriesScreen} 
+          options={{
+            title:"Categories",
+            headerStyle:{
+              backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : 'white',
+            },
+            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+          }}
+        />
+        <Stack.Screen name="CategoryMealsScreen" component={CategoryMealsScreen} options={{title:"Category Meal"}} />
+        <Stack.Screen name="MealDetailScreen" component={MealDetailScreen} options={{title:"Meal Detail"}}/>
       </Stack.Navigator>
     </NavigationContainer>
     
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { View, Text, StyleSheet, FlatList,TouchableOpacity } from 'react-native'
 import { CATEGORIES } from '../data/dummy-data'
 
-const renderGridItem = (itemData) => {
-    return <View style={styles.gridItem}><Text>{itemData.item.title}</Text></View>
-}
 
 
 const CategoriesScreen = props => {
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity style={styles.gridItem} onPress={()=>{
+                props.navigation.navigate({name: 'CategoryMealsScreen'})
+            }}>
+                <View>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
     return (
         <FlatList numColumns={2} data={CATEGORIES} renderItem={renderGridItem}/>
 
     )
 }
+
 
 const styles = StyleSheet.create({
     screen:{
