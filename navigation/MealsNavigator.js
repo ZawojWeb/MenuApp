2665193751c2d090c34bac61ca4b1026efc44009
Kacper Zawojski <zawojweb@gmail.com>
2021-08-01 15:38:29 +0200

    Make a navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
+import MealDetailScreen from "../screens/MealDetailScreen"
 
 const Stack = createStackNavigator();
 
@@ -10,8 +11,9 @@ export default function MealsNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="CategoryMealsScreen" component={CategoryMealsScreen} />
         <Stack.Screen name="CategoriesScreen" component={CategoriesScreen} />
+        <Stack.Screen name="CategoryMealsScreen" component={CategoryMealsScreen} />
+        <Stack.Screen name="MealDetailScreen" component={MealDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
     
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { View, Text,StyleSheet } from 'react-native'
-const CategoryMealsScreen = () => {
+import { View, Text,StyleSheet, Button } from 'react-native'
+const CategoryMealsScreen = props => {
     return (
         <View>
             <Text>Category Meals Screen</Text>
+            <Button title="Go to Details" onPress={()=>{
+                props.navigation.navigate({name: "MealDetailScreen"})
+            }}></Button>
         </View>
     )
 }
