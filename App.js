a9f849d6e546d0dc5510ea8b1b23596cbf411a19
Kacper Zawojski <zawojweb@gmail.com>
2021-08-02 12:40:12 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import MealsNavigator from './navigation/MealsNavigator';
+import { enableScreens } from 'react-native-screens';
+
+
+enableScreens();
 
 export default function App() {
   const [fontLoaded, setfontLoaded] = useState(false);
