4ddf19be1ea8e93d4da2169876ab7c9a234f3d21
Kacper Zawojski <zawojweb@gmail.com>
2021-08-08 21:45:47 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
@@ -12,14 +12,19 @@ enableScreens();
 export default function App() {
   const [fontLoaded, setfontLoaded] = useState(false);
 
-  const fetchFonts = () =>{
+
+
+
+
+
+  const fetchFonts = () => {
     Font.loadAsync({
       'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
       'open-sans-blod': require('./assets/fonts/OpenSans-Bold.ttf')
     });
   }
-  if(!fontLoaded){
-    return (<AppLoading startAsync={fetchFonts} onError={(err) => {console.log(err)}} onFinish={() => setfontLoaded(true)}/>)
+  if (!fontLoaded) {
+    return (<AppLoading startAsync={fetchFonts} onError={(err) => { console.log(err) }} onFinish={() => setfontLoaded(true)} />)
   }
 
   return (
